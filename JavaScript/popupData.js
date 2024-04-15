ac6694841bfa0220b68294cb3cf05413df337c6a
Khushil sheth <128523714+khushilsheth@users.noreply.github.com>
2024-04-15 19:05:02 +0530

    Add files via upload

---
+++ JavaScript/popupData.js
@@ -0,0 +1,7 @@
+export const popupData = {
+    'Transportation' : {
+        imgSrc: '../images/school bus-cuate 1.png',
+        content: 'Hello, Welcome to the onboarding of transportation.',
+        redirectLink: './Transportation.html'
+    }
+}
