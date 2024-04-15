be963fe6d2118252135af0587bf0af0d9d426cf4
Khushil sheth <128523714+khushilsheth@users.noreply.github.com>
2024-04-15 16:40:37 +0530

    Add accordion menu

--- JavaScript/popupData.js
+++ JavaScript/popupData.js
@@ -2,6 +2,6 @@ export const popupData = {
     'Transportation' : {
         imgSrc: '../images/school bus-cuate 1.png',
         content: 'Hello, Welcome to the onboarding of transportation.',
-        redirectLink: './1.1Step.html'
+        redirectLink: './Transportation.html'
     }
 }
