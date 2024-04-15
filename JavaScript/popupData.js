9bdc1159a782bccf55ccf6583d2781ca0644ec1b
Khushil sheth <128523714+khushilsheth@users.noreply.github.com>
2024-04-15 19:03:19 +0530

    Delete JavaScript directory

--- JavaScript/popupData.js
+++
@@ -1,7 +0,0 @@
-export const popupData = {
-    'Transportation' : {
-        imgSrc: '../images/school bus-cuate 1.png',
-        content: 'Hello, Welcome to the onboarding of transportation.',
-        redirectLink: './Transportation.html'
-    }
-}
